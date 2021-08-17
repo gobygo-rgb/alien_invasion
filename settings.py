fd8d73e94fe1e40c4cd56c36555081c4341b6dd8
gobygo <gobygo@outlook.com>
2021-08-17 22:19:22 +0800

    aliens move up and down

--- settings.py
+++ settings.py
@@ -4,7 +4,10 @@ class Settings:
         self.screen_height = 750
         self.bg_color = (230,230,230)
         self.ship_speed = 1.5
-        
+        self.alien_speed = 0.1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
+
         self.bullet_speed = 0.3
         self.bullet_width = 3
         self.bullet_height = 15
